6a5afb21dba5b55951f50d0275bea1832dbddd8a
kriszv2 <krisztian.valyi1@gmail.com>
2022-08-15 10:42:22 +0100

    last

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
-wrapper.style.transform = "translateX(-100vw)";
+const buyButton = document.querySelector(".productButton");
+const paymentModal = document.querySelector(".payment");
+const close = document.querySelector(".close");
+
 const products = [
   {
     id: 1,
@@ -79,8 +82,52 @@ const products = [
   },
 ];
 
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
 menuItems.forEach((item, index) => {
   item.addEventListener("click", () => {
+    //change the current slide
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
   });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+buyButton.addEventListener("click", () => {
+  paymentModal.style.display = "flex";
+});
+close.addEventListener("click", () => {
+  paymentModal.style.display = "none";
 });
